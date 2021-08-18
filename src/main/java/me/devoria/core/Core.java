fa37dc38484d8f2ef1335bc81ad68e84cbc00aaf
ptobey <patmictob@gmail.com>
2021-08-18 01:42:10 -0400

    readded a command

--- src/main/java/me/devoria/core/Core.java
+++ src/main/java/me/devoria/core/Core.java
@@ -2,6 +2,7 @@ package me.devoria.core;
 
 import me.devoria.core.commands.AttyMod;
 import me.devoria.core.commands.ClassCommand;
+import me.devoria.core.commands.ItemCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Core extends JavaPlugin {
@@ -19,6 +20,7 @@ public class Core extends JavaPlugin {
     }
     public void registerCommands() {
         this.getCommand("class").setExecutor(new ClassCommand());
+        this.getCommand("item").setExecutor(new ItemCommand());
         this.getCommand("reveal").setExecutor(new AttyMod());
     }
 
