f14782f1a03a6316c79b2ae8fe2f1d7d9464b1ac
isouza <isouza@missouriwestern.edu>
2021-08-17 19:52:11 -0500

    Custom bow test

---
+++ src/main/java/me/devoria/core/CustomBow/CustomBow.java
@@ -0,0 +1,30 @@
+package me.devoria.core.CustomBow;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.Arrays;
+
+public class CustomBow {
+
+
+    public ItemStack buildBow(){
+
+        ItemStack bow = new ItemStack(Material.PINK_WOOL);
+        bow.addUnsafeEnchantment(Enchantment.ARROW_FIRE,1);
+
+        ItemMeta bowMeta = bow.getItemMeta();
+
+        bowMeta.setDisplayName(ChatColor.WHITE + "" + "Strengthened Wooden Bow");
+        bowMeta.setLore(Arrays.asList(ChatColor.DARK_GRAY + "" + ChatColor.BOLD + "Created by the great power of PTO",
+                                                                                ChatColor.DARK_PURPLE + "Mythic Item"));
+        bow.setItemMeta(bowMeta);
+
+        return bow;
+    }
+
+}
