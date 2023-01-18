1b9d0eff631c346245520afa4aeb485cdb4c8e7a
SleepiiRaven <104394825+SleepiiRaven@users.noreply.github.com>
2023-01-17 19:09:38 -0800

    Revert "Fixed Custom Mobs"
    This reverts commit b9a605bcf17374e38548001febbe12bbe1cce936.

--- src/main/java/me/devoria/spells/imanity/humans/movement/Dash.java
+++ src/main/java/me/devoria/spells/imanity/humans/movement/Dash.java
@@ -3,8 +3,6 @@ package me.devoria.spells.imanity.humans.movement;
 import me.devoria.Devoria;
 import me.devoria.cooldowns.CooldownManager;
 import me.devoria.spells.Spell;
-import org.bukkit.Color;
-import org.bukkit.Particle;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.Vector;
@@ -13,23 +11,16 @@ public class Dash extends Spell {
     @Override
     public void cast(Player p, CooldownManager cooldownManager) {
         Vector velocity = p.getVelocity();
-        Vector direction = new Vector(p.getEyeLocation().getDirection().getX(), 0, p.getEyeLocation().getDirection().getZ()).multiply(20).setY(0.4);
+        Vector direction = new Vector(p.getEyeLocation().getDirection().getX(), 0, p.getEyeLocation().getDirection().getZ()).normalize().multiply(20);
+        direction.setY(0.4);
         p.setVelocity(direction);
-
         new BukkitRunnable() {
-            int ticks = 0;
-
             @Override
             public void run() {
-                ticks++;
-                if (ticks >= 3) {
-                    p.setVelocity(velocity);
-                    cancel();
-                }
-                p.getWorld().spawnParticle(Particle.REDSTONE, p.getLocation().add(0, 1, 0), 10, 0.2, 0.2, 0.2, 0.1, new Particle.DustOptions(Color.AQUA, 1));
-                p.getWorld().spawnParticle(Particle.SMOKE_NORMAL, p.getLocation().add(0, 1, 0), 10, 0.2, 0.2, 0.2, 0.1);
+                p.setVelocity(velocity);
             }
-        }.runTaskTimer(Devoria.getInstance(), 0L, 1L);
+        }.runTaskLater(Devoria.getInstance(), 5L);
+        //Particles
     }
 
     @Override
